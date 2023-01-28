1425dded5271a3bd53fcb7443c33e70b4b3e5f07
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2023-01-28 17:48:27 +0300

    Added initial files

---
+++ go.mod
@@ -0,0 +1,9 @@
+module website-checker-bot
+
+go 1.18
+
+require (
+	github.com/aws/aws-lambda-go v1.37.0 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	gopkg.in/tucnak/telebot.v2 v2.5.0 // indirect
+)
---
+++ src/bot.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	tb "gopkg.in/tucnak/telebot.v2"
+	"os"
+)
+
+func main() {
+	settings := tb.Settings{
+		Token:       os.Getenv("BOT_TOKEN"),
+		Synchronous: true,
+		Verbose:     true,
+	}
+	tgBot, err := tb.NewBot(settings)
+	if err != nil {
+		fmt.Println(err)
+		panic("can't create bot")
+	}
+	tgBot.Handle(tb.OnText, func(m *tb.Message) {
+		message := m.Text
+		_, err := tgBot.Send(m.Sender, message)
+		if err != nil {
+			fmt.Println(err)
+			panic("can't send message")
+		}
+	})
+	lambda.Start(func(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		var u tb.Update
+		if err = json.Unmarshal([]byte(req.Body), &u); err == nil {
+			tgBot.ProcessUpdate(u)
+		}
+		return events.APIGatewayProxyResponse{Body: "ok", StatusCode: 200}, nil
+
+	})
+}
